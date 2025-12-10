e750fc4cf51e4659ad6b969944147b1916d0780b
Rahul Mulik <rahul.mulik@prototechsolutions.com>
2025-12-10 17:01:08 +0530

    Project Initialised

---
+++ Spring_Postgres/src/test/java/com/rahul/SpringPostgres/SpringPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.SpringPostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
