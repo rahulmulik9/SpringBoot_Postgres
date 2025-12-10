8ac34a73d172d861311c00ffa19c11831de598fb
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-10 17:36:07 +0530

    Added student entity

---
+++ Spring_Postgres/src/main/java/com/rahul/SpringPostgres/entity/Student.java
@@ -0,0 +1,25 @@
+package com.rahul.SpringPostgres.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name",unique = true)
+    private String name;
+
+    @Column(name="marks",nullable = false)
+    private int marks;
+}
