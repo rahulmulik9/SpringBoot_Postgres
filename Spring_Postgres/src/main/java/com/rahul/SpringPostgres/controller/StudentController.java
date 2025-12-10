ea28871bdecc0064c3deb5205df121b67038d472
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-10 19:08:18 +0530

    Add StudentController

--- Spring_Postgres/src/main/java/com/rahul/SpringPostgres/controller/StudentController.java
+++ Spring_Postgres/src/main/java/com/rahul/SpringPostgres/controller/StudentController.java
@@ -1,7 +1,25 @@
 package com.rahul.SpringPostgres.controller;
 
 
+import com.rahul.SpringPostgres.dto.Studentdto;
+import com.rahul.SpringPostgres.serive.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employee")
 public class StudentController {
+    private StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<Studentdto> createStudent(@RequestBody Studentdto studentdto){
+         Studentdto studentdto1 = studentService.createStudent(studentdto);
+         return new ResponseEntity<>(studentdto1, HttpStatus.CREATED);
+    }
 }
